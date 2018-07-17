65ee888614f07f2bccdec2de3cfd7435f1f02198
Metawee Langka <m.langka93@gmail.com>
2018-07-17 17:08:06 +0200

    commit

--- CVE_Analysis.py
+++ CVE_Analysis.py
@@ -7,14 +7,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_csv('CVE_Cleaned.csv',
+dataSet = pd.read_csv('CVE_Cleaned.csv',
                       warn_bad_lines=True, 
                       error_bad_lines=False,
-                      delimiter=';')
+                      delimiter=';', decimal = ',')
 
 # Number of CVEs discovered by year
 
-discYear = data['discoveredYear']
+discYear = dataSet['discoveredYear']
 plt.title('Number of CVEs discovered by year')
 plt.xlabel('Discovered Year')
 plt.ylabel('Count')
@@ -23,9 +23,9 @@ discYear.value_counts().plot(kind='bar',
 plt.show()
 
 
-# Number of products in the database each year
+# Number of products in the dataSetbase each year
 
-numProduct = data.groupby('discoveredYear')['product_name'].nunique()
+numProduct = dataSet.groupby('discoveredYear')['product_name'].nunique()
 plt.title('Number of products discovered by year')
 plt.xlabel('Discovered Year')
 plt.ylabel('Number of Products')
@@ -35,7 +35,7 @@ plt.show()
 
 # Top 15 CVE by Vendor
 
-countVendor = data['vendor_name'].value_counts()
+countVendor = dataSet['vendor_name'].value_counts()
 plt.title('Top 15 CVEs found by Vendor')
 plt.xlabel('Count')
 plt.ylabel('Vendor Name')
@@ -45,28 +45,65 @@ plt.show()
 
 # Top 5 OS comparison 
 
-selectedOS = data[data.product_name.isin(['windows_10','iphone_os','linux_kernel','android','mac_os_x'])]
-plt.title('Top 5 OS Comparison')
+selectedOS = dataSet[dataSet.product_name.isin(['windows_10','iphone_os','linux_kernel','android','mac_os_x'])]
+plt.title('Top 5 OSs Comparison')
 selectedOS['product_name'].value_counts().plot(kind='pie', figsize=[10,10])
 plt.show()
 
 
-# Exploitability Vs. Impact Score by year 
-
-#exploitScr = data.groupby('discoveredYear')['V2_exploitabilityScore'].sum()
-#impactScr = data.groupby('discoveredYear')['V2_impactScore']
-#plt.title('Exploitability Vs. Impact Score by year')
-#plt.xlabel('Discovered Year')
-#plt.ylabel('Avg Score')
-
-
 # CVEs Found by Year
 
-cveDescCount = data.groupby('discoveredYear')['CWE_Description'].value_counts().unstack().fillna(0)
+cveDescCount = dataSet.groupby('discoveredYear')['CWE_Description'].value_counts().unstack().fillna(0)
 cveDescCount.tail(10).plot(kind='line', figsize=[10,10],legend=None)
 plt.title('Top CVEs found by Year')
 plt.xlabel('Discovered Year')
 plt.ylabel('Number of CVEs Found')
 plt.show()
+
+
+# Exploitability Vs. Impact Score by year 
+
+avgExploitScr = (dataSet.groupby('discoveredYear')['V2_exploitabilityScore'].sum())/dataSet.groupby('discoveredYear')['V2_exploitabilityScore'].count()
+avgImpactScr = (dataSet.groupby('discoveredYear')['V2_impactScore'].sum())/dataSet.groupby('discoveredYear')['V2_impactScore'].count()
+plt.figure(figsize= (10, 10))
+plt.title('Exploitability Vs. Impact Score by year')
+plt.xlabel('Discovered Year')
+plt.ylabel('Avg Score')
+plt.plot(avgExploitScr, color='blue', label='Exploitabitity Score')
+plt.plot(avgImpactScr, color='red',label='Impact Score')
+plt.legend()
+plt.show()
+
+
+# Average Base Score by Year
+
+sumBaseScr = dataSet.groupby('discoveredYear')['V2_baseScore'].sum()
+numBaseScr = dataSet.groupby('discoveredYear')['V2_baseScore'].count()
+avgBaseScr = sumBaseScr/numBaseScr
+avgTotal = sumBaseScr.sum()/numBaseScr.sum()
+plt.figure(figsize= (10, 10))
+plt.title('Average Base Score by year')
+plt.xlabel('Discovered Year')
+plt.ylabel('Avg Base Score')
+plt.plot(avgBaseScr)
+plt.axhline(y=avgTotal, color='green', linestyle='--')
+#plt.plot(avgTotal,color='green',linestyle='--')
+#plt.legend()
+plt.show()
+
+
+# CVEs by Companies and Products 
+
+selectedVendor = dataSet[dataSet.vendor_name.isin(['microsoft','apple','google','adobe','oracle','ibm','cisco'])]
+#y = (selectedOS2['product_name'].value_counts()*100)/selectedOS2['product_name'].value_counts().sum()
+
+
+#
+X = dataSet[['V2_exploitabilityScore', 'V2_impactScore']].astype(np.float)
+
+y = dataSet['V2_impactScore'].astype(np.float)
+
+pd.scatter_matrix(X, alpha = 0.3, figsize = (10,10), diagonal = 'kde')
+
 
 
